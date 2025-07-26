de77911df0a9011ed98f0a163111cbfba1eedb87
prashu 1815 <jsaiprasanth18@gmail.com>
2025-07-26 14:46:22 +0530

    Fix: corrected CarList.css path issue

--- src/components/Carlist.jsx
+++ src/components/Carlist.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../Styles/CarList.css';
+import '../Styles/Cariist.css';
 
 function CarList() {
   const [cars, setCars] = useState([]);
